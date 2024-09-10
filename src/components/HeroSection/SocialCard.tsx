688deb7c4be1c5f45a17e7133933b4318dd6ebe4
RitikJaiswal75 <ritik12103032000@gmail.com>
2024-09-10 21:52:14 +0530

    fix image styles in social card

--- src/components/HeroSection/SocialCard.tsx
+++ src/components/HeroSection/SocialCard.tsx
@@ -4,8 +4,8 @@ import Socials from './Socials'
 function SocialCard() {
     return (
         <div className="w-full sm:flex sm:w-3/4 border-2 border-slate-200 rounded-xl px-4 hover:scale-105 transition ease-in-out shadow-[.5rem_.5rem_1rem_rgb(79,76,76)] gap-4">
-            <div className='sm:w-2/5 py-4 max-h-[80vh]'>
-                <img src="/images/ritik_portrait.webp" alt="Ritik Jaiswal" className='sm:rounded-l-xl h-full' />
+            <div className='sm:w-2/5 py-4 max-h-[80vh] flex justify-center'>
+                <img src="/images/ritik_portrait.webp" alt="Ritik Jaiswal" className='rounded-xl h-[70vh]' />
             </div>
             <div className='py-2 flex flex-col h-[50%] my-auto'>
                 <AnimatedText text='Hello World!! Welcome to my space!' />
