738dedc821440e7260c49d15e0ef3cf95f303e38
Ritik Jaiswal <57758447+RitikJaiswal75@users.noreply.github.com>
2024-10-08 16:09:41 +0530

    Add json for contact card, social and resume downloadable (#2)
    * Add json for contact card, social and resume downloadable
    
    * Add robots.txt and some seo related things like keywords

--- src/components/DownloadResume.tsx
+++ src/components/DownloadResume.tsx
@@ -1,6 +1,8 @@
+import { portfolioData } from "../data/allData"
+
 function DownloadResume() {
     return (
-        <a className="h-[40px] bg-[#c79c18] text-white font-2xl font-bold flex justify-center items-center fixed right-2 top-2 sm:!bottom-2 rounded px-2" href="/documents/ritik_jaiswal_resume.pdf" target="_blank">
+        <a className="h-[40px] bg-[#c79c18] text-white font-2xl font-bold flex justify-center items-center fixed right-2 top-2 sm:!bottom-2 rounded px-2" href="/documents/resume.pdf" download={portfolioData.resume.downloadable ? `${portfolioData.name.replace(' ', '_')}_resume.pdf` : false} target="_blank">
             <i className="ri-download-2-fill"></i>
             <span>Resume</span>
         </a>
--- src/components/HeroSection/ContactCard.tsx
+++ src/components/HeroSection/ContactCard.tsx
@@ -1,9 +1,11 @@
+import { portfolioData } from "../../data/allData"
+
 function ContactCard() {
     return (
         <div className="w-full sm:w-1/4 border-2 border-slate-200 rounded-xl px-4 hover:scale-105 transition ease-in-out shadow-[-.5rem_.5rem_1rem_rgb(79,76,76)]">
             <div className="py-2 pt-4">
                 <img src="/images/profile_v1.jpg" className="rounded-xl w-2/3 mx-auto" alt="Ritik Jaiswal" />
-                <h1 className="text-white font-bold my-2 text-center text-2xl">Ritik Jaiswal</h1>
+                <h1 className="text-white font-bold my-2 text-center text-2xl">{portfolioData.name}</h1>
                 <div className="w-full border-t-2 my-2"></div>
             </div>
 
@@ -12,15 +14,15 @@ function ContactCard() {
                 <div className="my-2">
                     <div className="flex items-center break-all text-white gap-2 mb-4">
                         <i className="ri-mail-send-line text-white font-bold text-xl"></i>
-                        <a href="mailto:ritikjaiswalse@gmail.com" className="text-white font-bold text-lg">ritikjaiswalse@gmail.com</a>
+                        <a href={`mailto:${portfolioData.contactCard.email}`} className="text-white font-bold text-lg">{portfolioData.contactCard.email}</a>
                     </div>
                     <div className="flex items-center break-all text-white gap-2 mb-4">
                         <i className="ri-phone-find-line text-white font-bold text-xl"></i>
-                        <a href="tel:+918792083160" className="text-white font-bold text-lg">+91 879-2083-160</a>
+                        <a href={`tel:${portfolioData.contactCard.phone}`} className="text-white font-bold text-lg">{portfolioData.contactCard.phone}</a>
                     </div>
                     <div className="flex items-center break-all text-white gap-2 mb-4">
                         <i className="ri-map-pin-line text-white font-bold text-xl"></i>
-                        <a href="https://www.google.com/maps/place/Bellandur" className="text-white font-bold text-lg">Bellandur, Bangalore</a>
+                        <a href={portfolioData.contactCard.locationUrl} className="text-white font-bold text-lg">{portfolioData.contactCard.location}</a>
                     </div>
                 </div>
             </div>
