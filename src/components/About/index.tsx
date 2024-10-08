d274ca7f47bde7e996d73259eb883c3379fc1fd9
Ritik Jaiswal <ritik12103032000@gmail.com>
2024-10-08 23:46:30 +0530

    Add styles for big screens and max width

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -8,7 +8,7 @@ function About() {
     return (
         <section id='experience' className="w-full flex flex-col md:flex-row border-2 border-slate-200 rounded-xl px-4 shadow-[.5rem_.5rem_1rem_rgb(79,76,76)] sm:gap-8 my-8">
             <Toggles />
-            <div className="sm:w-4/5 max-h-[80vh] min-h-[55vh] overflow-y-auto">
+            <div className="sm:w-4/5 max-h-[80vh] min-h-[55vh] lg:min-h-[20vh] xl:min-h-[15vh] overflow-y-auto">
                 <Routes>
                     <Route path="/" element={<Navigate to='/experience' />} />
                     <Route path="/about" element={<AboutMe />} />
