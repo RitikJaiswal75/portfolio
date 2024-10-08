1856b4ade946be44d099d0233f61ec11fe43ba6b
Ritik Jaiswal <ritik12103032000@gmail.com>
2024-10-08 16:41:30 +0530

    Add sitemap and fix cls

--- src/components/HeroSection/SocialCard.tsx
+++ src/components/HeroSection/SocialCard.tsx
@@ -6,7 +6,7 @@ function SocialCard() {
     return (
         <div className="w-full sm:flex sm:w-3/4 border-2 border-slate-200 rounded-xl px-4 hover:scale-105 transition ease-in-out shadow-[.5rem_.5rem_1rem_rgb(79,76,76)] gap-4">
             <div className='sm:w-2/5 py-4 max-h-[80vh] flex justify-center'>
-                <img src="/images/profile_portrait.webp" alt={portfolioData.name} className='rounded-xl max-h-[70vh]' />
+                <img src="/images/profile_portrait.webp" alt={portfolioData.name} className='rounded-xl max-h-[70vh] min-h-[65vh]' />
             </div>
             <div className='sm:w-3/5 py-2 flex flex-col h-[50%] my-auto items-center justify-center'>
                 <AnimatedText text={portfolioData.social.catchPhrase} />
