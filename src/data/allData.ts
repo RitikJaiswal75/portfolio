8a920c089db90f36713bab082154ca7da48eb909
Ritik Jaiswal <ritik12103032000@gmail.com>
2025-05-15 09:35:40 +0530

    change location

--- src/data/allData.ts
+++ src/data/allData.ts
@@ -1,22 +1,22 @@
 export const portfolioData = {
-    name: 'Ritik Jaiswal',
-    contactCard: {
-        email: 'ritikjaiswalse@gmail.com',
-        phone: '+918792083160',
-        location: 'Bellandur, Bangalore',
-        locationUrl: 'https://www.google.com/maps/place/Bellandur'
+  name: "Ritik Jaiswal",
+  contactCard: {
+    email: "ritikjaiswalse@gmail.com",
+    phone: "+918792083160",
+    location: "Prayahraj, Uttar pradesh",
+    locationUrl: "https://www.google.com/maps/place/Bellandur",
+  },
+  social: {
+    catchPhrase: "Hello World!! Welcome to my space!",
+    availableSocials: {
+      linkedin: "https://www.linkedin.com/in/ritikjaiswal75/",
+      github: "https://www.github.com/ritikjaiswal75/",
+      twitter: "https://twitter.com/RitikJaiswal75",
+      instagram: "https://www.instagram.com/_ritikj1/",
+      youtube: "https://www.youtube.com/@ritikjaiswal75/",
     },
-    social: {
-        catchPhrase: 'Hello World!! Welcome to my space!',
-        availableSocials: {
-            linkedin: 'https://www.linkedin.com/in/ritikjaiswal75/',
-            github: 'https://www.github.com/ritikjaiswal75/',
-            twitter: 'https://twitter.com/RitikJaiswal75',
-            instagram: 'https://www.instagram.com/_ritikj1/',
-            youtube: 'https://www.youtube.com/@ritikjaiswal75/'
-        }
-    },
-    resume: {
-        downloadable: false
-    }
-}
+  },
+  resume: {
+    downloadable: false,
+  },
+};
